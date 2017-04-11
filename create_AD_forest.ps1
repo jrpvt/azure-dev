d0eaf986574019f680a3d0d0760a1bc1bd307925
jrpvt <jrpowell@gmail.com>
2017-04-11 16:29:06 -0400

    Updates

--- create_AD_forest.ps1
+++ create_AD_forest.ps1
@@ -1,14 +1,14 @@
 [CmdletBinding()]
 Param(
   [Parameter(Mandatory=$True,Position=1)]
-   [string]$domainName,
+  [string]$domainName,
 	
-   [Parameter(Mandatory=$True,Position=2)]
-   [string]$forestMode,
+  [Parameter(Mandatory=$True,Position=2)]
+  [string]$forestMode,
 
   [Parameter(Mandatory=$True,Position=3)]
-  [Security.SecureString]$password
+  [string]$password
 )
-
-Install-WindowsFeature AD-Domain-Services -IncludeManagementTools 2>> c:\log1.txt
-#Install-ADDSForest -DomainName $domainName -ForestMode $forestMode -SafeModeAdministratorPassword $password -Force | Out-File c:\log2.txt
+$securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName $domainName -ForestMode $forestMode -SafeModeAdministratorPassword $securePassword -Force
