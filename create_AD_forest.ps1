f3b40a9a6bbae1bf07a010d78d9c159e91ca1350
jrpvt <jrpowell@gmail.com>
2017-04-11 21:17:41 -0400

    Testing

--- create_AD_forest.ps1
+++ create_AD_forest.ps1
@@ -9,6 +9,6 @@ Param(
   [Parameter(Mandatory=$True,Position=3)]
   [string]$password
 )
-$securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+#$securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
-Install-ADDSForest -DomainName $domainName -ForestMode $forestMode -SafeModeAdministratorPassword $securePassword -Force
+#Install-ADDSForest -DomainName $domainName -ForestMode $forestMode -SafeModeAdministratorPassword $securePassword -Force
