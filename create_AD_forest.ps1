b354e0c0cf17aae89ee8dbb1830b4cb0fbaea8e4
jrpvt <jrpowell@gmail.com>
2017-04-12 14:02:06 -0400

    DNS zone

--- create_AD_forest.ps1
+++ create_AD_forest.ps1
@@ -7,8 +7,12 @@ Param(
   [string]$forestMode,
 
   [Parameter(Mandatory=$True,Position=3)]
-  [string]$dsrmPassword
+  [string]$dsrmPassword,
+
+  [Parameter(Mandatory=$True,Position=4)]
+  [string]$subnetAddressPrefix
 )
 $dsrmSecurePassword = ConvertTo-SecureString -String $dsrmPassword -AsPlainText -Force
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 Install-ADDSForest -DomainName $domainName -ForestMode $forestMode -SafeModeAdministratorPassword $dsrmSecurePassword -Force
+Add-DnsServerPrimaryZone -DynamicUpdate Secure -NetworkID $subnetAddressPrefix -ReplicationScope Domain
